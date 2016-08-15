0ac9d38e4439c94990a1f5e62d3a788d3b89872b
Sameer Malik <Sameer@Apples-MacBook-Air.local>
2016-08-15 18:25:05 +0530

    ok

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DribbleMy::Application.config.secret_key_base = '4c8426a85e46f71fb87be1bc88cc1e6b3cd16f2fb3d5a00da55179e2d1576e4e79a31fbf8c5af07e072cd36cbf06083c3615dd69a32af6058ec0fcb94e0b0353'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DribbleMy::Application.config.session_store :cookie_store, key: '_dribble_my_session'
